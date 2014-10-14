1fa3bf45f115183782e455d5e13c580baa60f8b6
Sergey Parshukov <sergey.parshukov@me.com>
2014-10-14 21:57:27 +0400

    Cube UV mapping

--- js/script.js
+++ js/script.js
@@ -8,19 +8,55 @@ var createScene = function() {
 	var scene = new BABYLON.Scene(engine);
 	scene.clearColor = labColor;
 
-	var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 0, BABYLON.Vector3.Zero(), scene);
+	var camera = new BABYLON.ArcRotateCamera("camera", 0, 0, 0, BABYLON.Vector3.Zero(), scene);
 	camera.setPosition(new BABYLON.Vector3(5, 5, 5));
 	camera.attachControl(canvas, false);
 
-	var light = new BABYLON.HemisphericLight("SkyLight", new BABYLON.Vector3(0, 1, 0), scene);
+	var light = new BABYLON.HemisphericLight("skyLight", new BABYLON.Vector3(0, 1, 0), scene);
 	light.intensity = 0.75;
 
-	var pointLight = new BABYLON.PointLight("PointLight", new BABYLON.Vector3(4, 3, 3.5), scene);
+	var pointLight = new BABYLON.PointLight("pointLight", new BABYLON.Vector3(4, -3, 3.5), scene);
 	pointLight.diffuse = labColor;
 	pointLight.specular = labColor;
 	pointLight.intensity = 0.75;
 
-	var box = BABYLON.Mesh.CreateBox("Box", 2.5, scene);
+	// var dynamicMaterial = new BABYLON.StandardMaterial("dynamicTextureMaterial", scene);
+	var box = BABYLON.Mesh.CreateBox("box", 2.5, scene);
+	// box.material = dynamicMaterial;
+
+	box.subMeshes = [];
+	box.subMeshes.push(new BABYLON.SubMesh(0, 0,  4,  0, 6, box));
+	box.subMeshes.push(new BABYLON.SubMesh(1, 4,  4,  6, 6, box));
+	box.subMeshes.push(new BABYLON.SubMesh(2, 8,  4, 12, 6, box));
+	box.subMeshes.push(new BABYLON.SubMesh(3, 12, 4, 18, 6, box));
+	box.subMeshes.push(new BABYLON.SubMesh(4, 16, 4, 24, 6, box));
+	box.subMeshes.push(new BABYLON.SubMesh(5, 20, 4, 30, 6, box));
+
+	var angles = [
+		Math.PI,  // right
+		0,  // left back
+		Math.PI / 2,  // left
+		Math.PI / 2,
+		0,  // top
+		0,
+	];
+
+	var boxMultiMaterial = new BABYLON.MultiMaterial("cubeMulti", scene);
+	for(var i = 0; i < 4; i++) {
+		var dynamicTexture = new BABYLON.DynamicTexture("dynamicTexture", 512, scene, true);
+		dynamicTexture.drawText("#partyprint", 50, 410, "bold 70px ComicSansMS", "#555", "#fff");
+
+		dynamicTexture.wAng = angles[i];
+
+		var dynamicMaterial = new BABYLON.StandardMaterial("dynamicTextureMaterial", scene);
+		dynamicMaterial.diffuseTexture = dynamicTexture;
+		boxMultiMaterial.subMaterials.push(dynamicMaterial);
+	}
+	var mat = new BABYLON.StandardMaterial("mat", scene);
+	mat.diffuseTexture = new BABYLON.Texture("img/texture.png", scene);
+	boxMultiMaterial.subMaterials.push(mat);
+
+	box.material = boxMultiMaterial;
 
 	return scene;
 };
